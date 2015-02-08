5b5346a42434d65d50c9d82b60fbf00f057447a7
karan173 <karan173@gmail.com>
2015-02-08 17:59:52 +0530

    Fixed console error in script.js

--- js/script.js
+++ js/script.js
@@ -1,6 +1,7 @@
 // http://groups.google.com/group/mozilla.dev.tech.dom/browse_thread/thread/7ecbbb066ff2027f
 // Martin Honnen
 //  http://JavaScript.FAQTs.com/ 
+
 var getLinks = function(rawHTML)
 {
 	var doc = document.createElement("html");
@@ -20,17 +21,19 @@ var getLinks = function(rawHTML)
 	return {"urls" : urls, "titles":titles};
 }
 var selection = window.getSelection();
-
-var range = selection.getRangeAt(0);
-if (range) 
+if(selection && selection.rangeCount > 0)
 {
-	var div = document.createElement('div');
-	div.appendChild(range.cloneContents());
-	var vs=div.innerHTML;
-	req = getLinks(vs);
-	console.log(req);
-	if(req.urls.length > 0)
+	var range = selection.getRangeAt(0);
+	if (range) 
 	{
-		chrome.runtime.sendMessage(req);
+		var div = document.createElement('div');
+		div.appendChild(range.cloneContents());
+		var vs=div.innerHTML;
+		req = getLinks(vs);
+		//console.log(req);
+		if(req.urls.length > 0)
+		{
+			chrome.runtime.sendMessage(req);
+		}
 	}
-} 
+}
