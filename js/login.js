5447b43f08238e59082074c20d058670749acbe7
karan173 <karan173@gmail.com>
2015-02-08 17:51:31 +0530

    Change welcome text on pocket login

--- js/login.js
+++ js/login.js
@@ -2,6 +2,10 @@ $(document).ready(function()
 {
 	Auth.getAccessToken(function()
 	{
-		$('#login_text').text('Congratulations ' + localStorage['username'] + '. You can now use the Batch Save Extension :). This extension is developed by karan173[ksb.nsit@gmail.com]');	
+		$('#login_text').html('<h3>Congratulations ' + localStorage['username'] + 
+		'. You can now use the Batch Save Extension :). This extension is developed by karan173[ksb.nsit@gmail.com].'+
+		'<br>Please review the extension on <a href = '+
+		'"https://chrome.google.com/webstore/detail/batch-save-pocket/ihhiomekhplpdojbcaniaglcfopckaef">'+
+		'Chrome web store</a> if you like it</h3>');	
 	});
 });
